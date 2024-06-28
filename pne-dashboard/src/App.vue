9fdbea3cdadfc72cecad3e38a464dd0c117879a7
Raghd Alshoufani <121362316+Raghdkun@users.noreply.github.com>
2024-06-28 13:27:24 -0400

    vue project

---
+++ pne-dashboard/src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import  CardView  from './components/card/CardView.vue'
+</script>
+
+<template>
+<CardView></CardView>
+
+</template>
+
+<script>
+export default {
+  components : {
+    CardView
+  },
+  data() {
+    return {
+      list: [
+        {name: 'alen', position: 'GM'},
+        {name: 'emma', position: 'GM'},
+
+      ]
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pne-dashboard/src/components/card/CardView.vue
@@ -0,0 +1,37 @@
+<script setup>
+import CardComp from './CardComp.vue'
+</script>
+
+<template>
+    <div class="card">
+        <p><CardComp name="lucas" position="Admin"></CardComp></p>
+        <p><CardComp name="bigsam" position="Admin"></CardComp></p>
+        <div >
+            <li v-for-list="lists">
+                <CardComp name="{{list.name}}" position="{{list.position}}"></CardComp>
+            </li>
+        </div>
+
+    </div>
+    
+</template>
+
+
+<script>
+export default {
+    components : {
+        CardComp, 
+    },
+
+}
+</script>
+
+
+<style scoped>
+.card{
+    margin: 20px;
+    margin-left: 0px;
+    margin-right: 0px;
+
+}
+</style>
